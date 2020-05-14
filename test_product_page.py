aa5cc1ef1423c5606318c0ec398c71dd79f51e69
NazarovIL <nazarovil2011@yandex.ru>
2020-05-14 09:55:54 +0300

    Added universal transition to the login page

--- test_product_page.py
+++ test_product_page.py
@@ -17,44 +17,54 @@ import time
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    main_page_element = product_page.should_be_message_about_product()
-    product_page.should_be_add_button()
-    product_page.go_to_product_page()
-    product_page.solve_quiz_and_get_code()
-    product_page_element = product_page.should_be_message_about_product_in_basket()
-    product_page.element_comparison(main_page_element, product_page_element)
+    page = ProductPage(browser, link)
+    page.open()
+    main_page_element = page.should_be_message_about_product()
+    page.should_be_add_button()
+    page.go_to_product_page()
+    page.solve_quiz_and_get_code()
+    product_page_element = page.should_be_message_about_product_in_basket()
+    page.element_comparison(main_page_element, product_page_element)
 
 #Проверка отсутствия элемента (в данном случае он здесь есть)
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_add_button()
-    product_page.go_to_product_page()
-    product_page.solve_quiz_and_get_code()
-    product_page.should_not_be_success_message()
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_button()
+    page.go_to_product_page()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
 
-#Проверка отсутствия элемента (в данном случае его здесь нет)
+#Проверка отсутствия элемента (в данном случае его здесь нет) - тест проходит
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_not_be_success_message()
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
 
 #проверка исчезновение элемента (в данном случае он не здесь не исчезает)
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_add_button()
-    product_page.go_to_product_page()
-    product_page.solve_quiz_and_get_code()
-    product_page.should_be_success_message()
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_button()
+    page.go_to_product_page()
+    page.solve_quiz_and_get_code()
+    page.should_be_success_message()
 
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
-
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    page.go_to_login_page()
